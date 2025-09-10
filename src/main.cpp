41fdb25a5acf9bdf4e45bbe151d4877ef29d7e4f
gwoth-design <85754040+gwoth-design@users.noreply.github.com>
2025-09-11 00:02:52 +0100

    and or not

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,5 @@
 #include "../headers/logicGates.h"
 
 int main() {
-	std::cout << LOGICGATES::AND(0, 1) << "\n";
-	std::cout << LOGICGATES::OR(0, 0) << "\n";
-	std::cout << LOGICGATES::NOT(1) << "\n";
-
 	return 0;
 }
